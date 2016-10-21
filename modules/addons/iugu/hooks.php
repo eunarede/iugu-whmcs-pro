dd8894db82e76e8813d47c61d8ca50be6ada4751
EunaRede <rede@eunarede.com>
2016-10-21 12:06:20 -0200

    Criação dos gatilhos

---
+++ modules/addons/iugu/hooks.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ *
+ * @ IUGU HOOKS FOR WHMCS
+ *
+ * @ Version  : 6.X
+ * @ Author   : EUNAREDE
+ * @ Release on : 2016-10-21
+ * @ Website  : http://www.eunarede.com
+ *
+ * */
+if (!defined( "WHMCS" )) {
+	exit( "This file cannot be accessed directly" );
+}
+
+// Gatilho para criação do cliente na Iugu para cobranças futuras
+
+function hook_create_client($vars){
+
+
+}
+
+ ?>
